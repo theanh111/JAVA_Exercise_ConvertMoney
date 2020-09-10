5d7517a8254abc3505d9ec5caa627a4932178b54
anh.tran(c0720i1) <69283798+theanh111@users.noreply.github.com>
2020-09-10 10:58:39 +0700

    bai tap Chuyen doi tien te

--- src/CovertMoney.java
+++ src/CovertMoney.java
@@ -4,8 +4,8 @@ public class CovertMoney {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Convert USD to VND:");
-        int number = input.nextInt();
-        int rate = 23000;
+        double number = input.nextInt();
+        double rate = 23000;
         System.out.println("Converted: " + number * rate + " VND");
     }
 }
